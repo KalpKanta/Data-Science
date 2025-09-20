1333b789a93cf7848db421a77780623e67ea1c37
KalpKanta <Exoplayz72@gmail.com>
2025-09-20 19:09:22 +0100

    Add files via upload

--- Lesson1.py
+++ Lesson1.py
@@ -47,16 +47,45 @@ print(z2.shape)
 print(o1.size)
 print(z1.size)
 print(z2.size)
-#size gves the total amount of numbers in an array
+#size gves the total amount of values in an array
 
 array = np.arange(1,51,5)
 print(array)
 
 array2 = np.linspace(10, 20, 6)
 print(array2)
-
+ 
 array1 = np.random.randint(1,100,5)
 print(array1)
 
 array3 = array2.reshape((3,2))
 print(array3)
+
+#sorting - 
+array4 = np.random.randint(1, 100, 10)
+print(array4)
+array4n = np.sort(array4)
+print(array4n)
+
+#slicing - 
+print(array4[2:5:1])
+print(array4[0:5:1])
+print(array4[:5:])
+print(array4[::])
+print(array4[::-1])
+
+#conditional slicing - 
+print(array4[[3,4,7,1,2,5]]) #not conditional slicing (selective indexes)
+print(array4[array4%2 == 0])
+print(array4[(array4>50) & (array4%2==0)])
+array5 = np.arange(1, 9)
+array5 = array5.reshape(2,4)
+print(array5[0, 1:3])
+
+x = np.arange(1,5)
+y = np.arange(11,15)
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
