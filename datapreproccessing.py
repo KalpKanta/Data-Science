ec980bf16d18f2ab6119cdec10cec11d04bfb19a
KalpKanta <Exoplayz72@gmail.com>
2025-12-14 14:09:33 +0000

    Add files via upload

--- datapreproccessing.py
+++ datapreproccessing.py
@@ -29,11 +29,28 @@ le = LabelEncoder()
 y = le.fit_transform(y)
 print(y)
 
+#one-hot encoding
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+a = ColumnTransformer(transformers = [("encoder", OneHotEncoder(), [0])], remainder = "passthrough")
+X = a.fit_transform(X)
+print(X)
+
 from sklearn.model_selection import train_test_split
 xtrain, xtest, ytrain, ytest = train_test_split(X, y, train_size = 0.5, random_state = 6)
 print(xtrain)
 
 print(xtrain, ytrain)
+
+#Scaling - Standard scaling
+#z=(x−μ)/σ
+from sklearn.preprocessing import StandardScaler
+s = StandardScaler()
+xtrain[:, 3: ] = s.fit_transform(xtrain[:, 3:])
+xtest[:, 3: ] = s.transform(xtest[:, 3: ])
+print(xtrain)
+print(xtest)
+
 
 
 #fit - providing the achine with info then the machine tries to learn from it
