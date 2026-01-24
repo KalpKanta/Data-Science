13ffcc5a969e030c9804592602721d8d478a7130
KalpKanta <Exoplayz72@gmail.com>
2026-01-24 18:59:16 +0000

    Add files via upload

--- timebasedanalysis.py
+++ timebasedanalysis.py
@@ -10,4 +10,19 @@ print(data[["New_cases", "DateReported"]].groupby("DateReported").sum())
 print(data[["New_deaths", "DateReported"]].groupby("DateReported").sum())
 I = data.loc[data["Country"] == "India", :]
 In = I.loc[(I["DateReported"].dt.year == 2021) & (I["DateReported"].dt.month == 4), :]
-print(In)
+#print(In)
+print("..................................................................................................")
+
+
+#Ind = I.loc[(I["DateReported"].dt.year == 2021) & (I["DateReported"].dt.month == 4) & (I["New_cases"] > 0), :]
+Ind = In.loc[(In["New_cases"] > 0), "New_cases"].sum()
+#print(Ind)
+
+Indi = I.loc[(I["DateReported"].dt.year == 2020), :]
+#print(Indi)
+
+m = I.groupby(I["DateReported"].dt.year)["New_deaths"].mean().nlargest(2)
+print(m)
+
+m = I.groupby([I["DateReported"].dt.year, I["DateReported"].dt.month])["New_deaths"].sum()
+print(m)
